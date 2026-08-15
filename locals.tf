a3951c6f8096af1c0d3f9cdb4573a60f42c11dbf
P Hemanth Kumar <80844476+hemanth4134@users.noreply.github.com>
2026-08-16 02:22:36 +0530

    Add locals configuration for project and environment

---
+++ locals.tf
@@ -0,0 +1,11 @@
+# locals.tf
+
+locals {
+  name = "${var.project_name}-${var.environment}"
+
+  common_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
