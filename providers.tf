39d3b06fced8f7f071a375d3beb8063bc43bb063
P Hemanth Kumar <80844476+hemanth4134@users.noreply.github.com>
2026-08-16 02:20:07 +0530

    Add AWS provider configuration in providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+# providers.tf
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
